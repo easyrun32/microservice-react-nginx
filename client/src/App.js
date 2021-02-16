4f41d0659f6aceafc32e145a344f37b498cbab33
bryan aviles <easyrun32@gmail.com>
2021-02-16 02:53:40 -0500

    making request

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,23 @@
+import { useEffect, useState } from "react";
 import axios from "axios";
 const App = () => {
-  return <div>DOPE</div>;
+  const [user, setUser] = useState({});
+  const [task, setTask] = useState({});
+  useEffect(() => {
+    axios.get("/api/users").then((res) => {
+      setUser(res.data);
+    });
+    axios.get("/api/task").then((res) => {
+      setTask(res.data);
+    });
+  }, []);
+
+  return (
+    <div>
+      Hello <b />
+      {user.server} server and {task.server} server
+    </div>
+  );
 };
 
 export default App;
